eab9967b9342af051e50620b3ba5329ff4977d27
hawk2 <141938812+hawk2@users.noreply.github.com>
2023-08-17 18:24:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/story', methods=['POST'])
 def story():
