91f0977a1020b37760dd01b5da51cbc2b099e51b
hawk2 <141938812+hawk2@users.noreply.github.com>
2023-11-04 14:26:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, jsonify
 import openai
-import time
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='public', template_folder='public')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/')
 def index():
@@ -10,48 +11,48 @@ def index():
 
 @app.route('/story', methods=['POST'])
 def story():
-    name = request.form['name']
-    hobbies = request.form['hobbies']
-    friends = request.form['friends']
-    
-    print("Generating...")
-    time.sleep(5)
-    print("You can now safely leave this tab")
+    try:
+        name = request.form['name']
+        hobbies = request.form['hobbies']
+        friends = request.form['friends']
+        storyAbout = request.form['storyAbout']
+        ageRange = request.form['ageRange']
 
-    story_response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content": "You are a children story teller. You write in standard English. You use perfect grammar. You must use capital letters at the beginning of each sentence."},
-            {"role": "user", "content": f"Write a story about {name}. {name} likes to {hobbies}. {name}'s best friends are/is {friends}."},
-        ]
-    )
+        story_response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are a children's story teller. You write in standard English. You use perfect grammar. You must use capital letters at the beginning of each sentence."},
+                {"role": "user", "content": f"Write a story about {name} who is in the age range of {ageRange}. {name} likes to {hobbies}. {name}'s best friends are {friends}. The story should be about {storyAbout}."},
+            ]
+        )
 
-    story = story_response['choices'][0]['message']['content']
+        story_text = story_response['choices'][0]['message']['content']
 
-    # Generate a DALL·E prompt
-    prompt_response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content": "You read stories and then generate a prompt for DALL·E about the stories. Must be kid friendly. Clearly specify no text. Use a cartoon style."},
-            {"role": "user", "content": f"Make a DALL·E prompt for this story: {story}"},
-        ]
-    )
+        prompt_response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You read stories and then generate a prompt for DALL·E about the stories. Must be kid-friendly. Clearly specify no text. Use a cartoon style."},
+                {"role": "user", "content": f"Make a DALL·E prompt for this story: {story_text}"},
+            ]
+        )
 
-    iprompt = prompt_response['choices'][0]['message']['content']
+        iprompt = prompt_response['choices'][0]['message']['content']
 
-    # Generate an image
-    image_response = openai.Image.create(
-        prompt=iprompt,
-        n=1,
-        size="1024x1024"
-    )
+        image_response = openai.Image.create(
+            prompt=iprompt,
+            n=1,
+            size="1024x1024"
+        )
 
-    # Save the URL of the generated image
-    image_url = image_response['data'][0]['url']
+        image_url = image_response['data'][0]['url']
 
-    return render_template('story.html', story=story)
+        return jsonify({
+            'story': story_text,
+            'iprompt': iprompt,
+            'image_url': image_url
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    openai.api_key = 'Your API key here'
     app.run(debug=True)
-
